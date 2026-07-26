9d42166645b69c72cba0896c4eeadaac576a84bb
MARCELLOORIZIO <MARCELLOORIZIO@YAHOO.IT>
2026-07-26 17:41:09 +0200

    Add temporal video probe to live screen analysis

--- lib/hcv_live_screen_probe.dart
+++ lib/hcv_live_screen_probe.dart
@@ -6,8 +6,10 @@ import 'dart:math';
 import 'package:camera/camera.dart';
 
 import 'hcv_active_display_classifier.dart';
+import 'hcv_display_risk_fusion.dart';
 import 'hcv_scene_geometry_classifier.dart';
 import 'hcv_scene_decision_fusion.dart';
+import 'hcv_temporal_capture_probe.dart';
 
 part 'hcv_live_screen_probe_core.dart';
 part 'hcv_live_screen_probe_sampling.dart';
