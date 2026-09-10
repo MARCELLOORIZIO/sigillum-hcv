5cc60b775f1fda858fafa50a6f2dc3b920bb7fca
MARCELLOORIZIO <MARCELLOORIZIO@YAHOO.IT>
2026-09-10 12:32:36 +0200

    test: use SIGILLUM package import for reflective ML guard

--- test/reflective_planar_ml_guard_regression_test.dart
+++ test/reflective_planar_ml_guard_regression_test.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:hcv_app/hcv_display_risk_fusion.dart';
+import 'package:sigillum_iphone/hcv_display_risk_fusion.dart';
 
 Map<String, dynamic> _frame(String predictedClass) => {
       'predictedClass': predictedClass,
